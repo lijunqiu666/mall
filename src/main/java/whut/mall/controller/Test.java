b5118ac1182b39ab947e5517d1a00e6399ef0b90
ljq <2502945869@qq.com>
2020-08-20 17:04:01 +0800

    完善配置文件 添加测试类

---
+++ src/main/java/whut/mall/controller/Test.java
@@ -0,0 +1,23 @@
+package whut.mall.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * mall
+ * 2020/8/20 16:58
+ * 测试类
+ *
+ * @author 李俊秋
+ **/
+@RestController
+@RequestMapping("/user")
+
+public class Test {
+    @GetMapping("/test")
+    public String test(){
+        return "hello";
+    }
+}
